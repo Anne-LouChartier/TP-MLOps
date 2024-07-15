2abaf15fb812d8a4397776696950661f89d66bbb
Anne-LouChartier <94170732+Anne-LouChartier@users.noreply.github.com>
2024-07-15 14:06:24 +0200

    Update ssh.tf

--- ssh.tf
+++ ssh.tf
@@ -3,24 +3,19 @@ resource "random_pet" "ssh_key_name" {
     separator = ""
 }
 
-resource "azapi_resource_action" "ssh_public_key_gen" {
-    type = ""
-    resource_id = azapi_resource.ssh_public_key.id
-    action = "GenerateKeyPair"
-    method = "POST"
-
-    response_export_values = ["PublicKey", "privateKey"]
+resource "azurerm_ssh_public_key" "ssh_public_key" {
+  name                = random_pet.ssh_key_name.id
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azapi_resource" "ssh_public_key" {
-    type = "Microsoft.Compute/ssh"
-    resource_id = 
-    action =
-    method =
-
-    response_export_values = ["PublicKey", "privateKey"]
+resource "azurerm_ssh_public_key" "ssh_key" {
+  name                = "${random_pet.ssh_key_name.id}-key"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  key_data            = file("${var.ssh_private_key_path}.pub")
 }
 
 output "key_data" {
-    value = jsondecode(azapi_resource_action.ssh_public_key_gen.output).publicKey
+  value = azurerm_ssh_public_key.ssh_key.public_key
 }
